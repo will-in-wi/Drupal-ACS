ba803fc2ff23456b1f9ade2c81306b9eb0c42af4
William Johnston <will@johnstonclan.net>
2013-10-30 21:07:19 -0500

    New API methods.

--- lib/Drupal/ACSSync/Sync.php
+++ lib/Drupal/ACSSync/Sync.php
@@ -5,8 +5,7 @@ namespace Drupal\ACSSync;
 /**
 * ACS Sync Functions
 */
-class Sync
-{
+class Sync {
 	protected $site_number;
 	protected $username;
 	protected $password;
@@ -17,14 +16,13 @@ class Sync
 		$this->password = $password;
 	}
 
-	public function api_call($params = array())
-	{
+	public function api_call($endpoint, $params = array()) {
 		$headers = [
 			'Authorization' => 'Basic ' . base64_encode($this->username . ':' . $this->password),
 		];
 
 		// Get the first page of results.
-		$result = drupal_http_request('https://secure.accessacs.com/api_accessacs_mobile/v2/' . $this->site_number . '/individuals?' . http_build_query($params), [
+		$result = drupal_http_request('https://secure.accessacs.com/api_accessacs_mobile/v2/' . $this->site_number . '/' . $endpoint . '?' . http_build_query($params), [
 			'headers' => $headers,
 		]);
 
@@ -34,5 +32,21 @@ class Sync
 		}
 
 		return $result->data;
+	}
+
+	public function get_people($query, $limit=10, $page=1) {
+		return $this->api_call('individuals', [
+			'q' => $query,
+			'pageSize' => $limit,
+			'pageIndex' => $page-1, // 0 indexed.
+		]);
+	}
+
+	public function get_all_people($limit=10, $page=1) {
+		return $this->get_people('%', $limit, $page);
+	}
+
+	public function get_person($id) {
+		return $this->api_call('individuals/' . $id);
 	}
 }
